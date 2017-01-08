4fb73eace22f08175b70542802a728e096fb5660
Andrey Mensky <men232@bigmir.net>
2017-01-08 20:07:00 +0200

    compile script

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-require('babel-register');
-require('./module');
+'use strict';
+
+import ApiError from './ApiError';
+import Context from './Context';
+import Router from './Router';
+import Server from './Server';
+
+export {Server, Router, Context, ApiError};
--- src/module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import ApiError from './ApiError';
-import Context from './Context';
-import Router from './Router';
-import Server from './Server';
-
-export {Server, Router, Context, ApiError};
